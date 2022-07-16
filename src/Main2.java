1eb43ae010aed2fffa3abd9f133e52c9bf621063
perkles <suelane.vieira@gmail.com>
2022-07-16 01:53:50 -0300

    first commit

---
+++ src/Main2.java
@@ -0,0 +1,54 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055600L;
+
+        //float f1 = 4.5;
+        float f2 = 10.668f;
+
+        double d1 = 65.69;
+        double d2 = 99.04d;
+
+        char c1 = 'w';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "ag dgb se se et t KNBJ8JBB &* &%& 75894389";
+
+        //String tdl = "09/02/1981";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
